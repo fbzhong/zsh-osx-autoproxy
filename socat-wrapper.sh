d4836bf73a8a1fa95992a72cbd15c565d3f5ffbe
Robin Zhong <robin@robinzhong.co>
2021-05-07 13:38:32 +0800

    Fix empty proxy config in socat_proxy env.

--- socat-wrapper.sh
+++ socat-wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "${SOCAT_PROXY:-$socat_proxy}" == "" ]; then
+if [ "${SOCAT_PROXY:-$socat_proxy}" == "" ] || [ "${SOCAT_PROXY:-$socat_proxy}" == "proxy:" ]; then
     socat - tcp-connect:$1:$2
 else
     socat - ${SOCAT_PROXY:-$socat_proxy}:$1:$2,${SOCAT_PROXY_PORT:-$socat_proxy_port}

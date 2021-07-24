fcfe3774c6dbdaca9e22cd686d241abb0c9d38dd
Robin Zhong <robin@robinzhong.co>
2021-07-25 01:01:33 +0800

    Fix empty env when proxy is disabled.

--- zsh-osx-autoproxy.plugin.zsh
+++ zsh-osx-autoproxy.plugin.zsh
@@ -23,7 +23,6 @@ proxy () {
         __ZSH_OSX_AUTOPROXY_HTTP_PROXY_PORT=${${__ZSH_OSX_AUTOPROXY_SCUTIL_PROXY#*HTTPPort : }[(f)1]}
         export http_proxy="http://${__ZSH_OSX_AUTOPROXY_HTTP_PROXY_SERVER}:${__ZSH_OSX_AUTOPROXY_HTTP_PROXY_PORT}"
         export HTTP_PROXY="${http_proxy}"
-        export http_proxy_address="${__ZSH_OSX_AUTOPROXY_HTTP_PROXY_SERVER}:${__ZSH_OSX_AUTOPROXY_HTTP_PROXY_PORT}"
     fi
     # https_proxy
     if (( $__ZSH_OSX_AUTOPROXY_HTTPS_PROXY_ENABLED )); then
@@ -39,32 +38,38 @@ proxy () {
         export ftp_proxy="http://${__ZSH_OSX_AUTOPROXY_FTP_PROXY_SERVER}:${__ZSH_OSX_AUTOPROXY_FTP_PROXY_PORT}"
         export FTP_PROXY="${ftp_proxy}"
     fi
-    # all_proxy
-    # disable socks proxy for all_proxy env.
-    # if (( $__ZSH_OSX_AUTOPROXY_SOCKS_PROXY_ENABLED )); then
-        # __ZSH_OSX_AUTOPROXY_SOCKS_PROXY_SERVER=${${__ZSH_OSX_AUTOPROXY_SCUTIL_PROXY#*SOCKSProxy : }[(f)1]}
-        # __ZSH_OSX_AUTOPROXY_SOCKS_PROXY_PORT=${${__ZSH_OSX_AUTOPROXY_SCUTIL_PROXY#*SOCKSPort : }[(f)1]}
-        # export all_proxy="socks5://${__ZSH_OSX_AUTOPROXY_SOCKS_PROXY_SERVER}:${__ZSH_OSX_AUTOPROXY_SOCKS_PROXY_PORT}"
-        # export ALL_PROXY="${all_proxy}"
-    # elif (( $__ZSH_OSX_AUTOPROXY_HTTP_PROXY_ENABLED )); then
+
+    if (( $__ZSH_OSX_AUTOPROXY_HTTP_PROXY_ENABLED )); then
+        http_proxy_address="${__ZSH_OSX_AUTOPROXY_HTTP_PROXY_SERVER}:${__ZSH_OSX_AUTOPROXY_HTTP_PROXY_PORT}"
+
         export all_proxy="${http_proxy}"
         export ALL_PROXY="${all_proxy}"
-    # fi
 
-    export rsync_proxy="${http_proxy_address}"
-    export RSYNC_PROXY="${http_proxy_address}"
+        export rsync_proxy="${http_proxy_address}"
+        export RSYNC_PROXY="${http_proxy_address}"
+    fi
 
-    export socat_proxy="proxy:${__ZSH_OSX_AUTOPROXY_HTTPS_PROXY_SERVER}"
-    export socat_proxy_port="proxyport=${__ZSH_OSX_AUTOPROXY_HTTPS_PROXY_PORT}"
+    if (( $__ZSH_OSX_AUTOPROXY_HTTPS_PROXY_ENABLED )); then
+        http_proxy_address="${__ZSH_OSX_AUTOPROXY_HTTPS_PROXY_SERVER}:${__ZSH_OSX_AUTOPROXY_HTTPS_PROXY_PORT}"
 
-    export SOCAT_PROXY="${socat_proxy}"
-    export SOCAT_PROXY_PORT="${socat_proxy_port}"
+        export all_proxy="${http_proxy}"
+        export ALL_PROXY="${all_proxy}"
 
-    export GIT_PROXY_COMMAND="${HOME}/.oh-my-zsh/custom/plugins/zsh-osx-autoproxy/socat-wrapper.sh"
+        export rsync_proxy="${http_proxy_address}"
+        export RSYNC_PROXY="${http_proxy_address}"
+
+        export socat_proxy="proxy:${__ZSH_OSX_AUTOPROXY_HTTPS_PROXY_SERVER}"
+        export socat_proxy_port="proxyport=${__ZSH_OSX_AUTOPROXY_HTTPS_PROXY_PORT}"
+
+        export SOCAT_PROXY="${socat_proxy}"
+        export SOCAT_PROXY_PORT="${socat_proxy_port}"
+
+        export GIT_PROXY_COMMAND="${HOME}/.oh-my-zsh/custom/plugins/zsh-osx-autoproxy/socat-wrapper.sh"
+    fi
 }
 
 noproxy () {
-    unset {http,https,ftp,rsync,all,}_proxy {HTTP,HTTPS,FTP,RSYNC,ALL}_PROXY socat_proxy{,_port} SOCAT_PROXY{,_PORT} GIT_PROXY_COMMAND http_proxy_address
+    unset {http,https,ftp,rsync,all,}_proxy {HTTP,HTTPS,FTP,RSYNC,ALL}_PROXY socat_proxy{,_port} SOCAT_PROXY{,_PORT} GIT_PROXY_COMMAND
 }
 
 # enable proxy env by default.

ff1af2ea7dce2194903de0fbac2d88cbee404c79
Robin Zhong <robin@robinzhong.co>
2022-01-16 22:11:28 +0800

    Add path.

--- socat-wrapper.sh
+++ socat-wrapper.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PATH=$PATH:/usr/local/bin:/opt/local/bin:/opt/homebrew/bin
+
 if [[ $1 == 10.* ]] || [[ $1 == 192.168.* ]] || [ "${SOCAT_PROXY:-$socat_proxy}" == "" ] || [ "${SOCAT_PROXY:-$socat_proxy}" == "proxy:" ]; then
     socat - tcp-connect:$1:$2
 else

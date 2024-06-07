d79a2b3f3b869cb35e1e2559306f20ec1fabb391
Robin Zhong <robin@robinzhong.co>
2024-06-07 10:28:44 +0800

    skip 172.16~172.31 private ip range.

--- socat-wrapper.sh
+++ socat-wrapper.sh
@@ -4,7 +4,7 @@ export PATH=$PATH:/opt/homebrew/bin:/opt/local/bin
 
 SOCAT_ARGS="-T180"
 
-if [[ $1 == 10.* ]] || [[ $1 == 192.168.* ]] || [ "${SOCAT_PROXY:-$socat_proxy}" == "" ] || [ "${SOCAT_PROXY:-$socat_proxy}" == "PROXY:" ]; then
+if [[ $1 == 10.* ]] || [[ $1 == 192.168.* ]] || [[ $1 =~ ^172\.(1[6-9]|2[0-9]|3[01])\..* ]] || [ "${SOCAT_PROXY:-$socat_proxy}" == "" ] || [ "${SOCAT_PROXY:-$socat_proxy}" == "PROXY:" ]; then
     socat $SOCAT_ARGS - TCP-CONNECT:$1:$2
 else
     socat $SOCAT_ARGS - ${SOCAT_PROXY:-$socat_proxy}:$1:$2,${SOCAT_PROXY_PORT:-$socat_proxy_port},retry=3
